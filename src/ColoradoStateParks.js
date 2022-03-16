58f7258a58d19545b713e62956f549955b715750
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-16 14:14:43 -0700

    done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+// import { trees, wildlife, elevation } from "./parks/RockyMountain";
+import {
+  trees as rockyMtnTrees,
+  wildlife as rockyMtnWildlife,
+  elevation as rockyMtnElevation,
+} from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  rockyMtnWildlife();
+  rockyMtnElevation();
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+      {rockyMtnTrees}
+    </div>
+  );
 }
+export default ColoradoStateParks;
